b4919d3cebbd2c0bf3c5d9457dbca56c61b90e91
Brian Ito <brian.oliveira100@gmail.com>
2021-07-31 19:29:58 -0400

    add cors

--- next.config.js
+++ next.config.js
@@ -1,3 +1,30 @@
 module.exports = {
-  reactStrictMode: true,
+  experimental: {
+    async headers() {
+      return [
+        {
+          source: '/(.*)?',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: '*'
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+            },
+          ]
+        }
+      ]
+    },
+    // temporary until https://github.com/zeit/next.js/pull/11755
+    async rewrites() {
+      return [
+        {
+          source: '/:path*',
+          destination: '/:path*'
+        }
+      ]
+    }
+  }
 }
